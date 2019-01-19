f94f2392c299da786099c264cd8ff2d85dd86dcd
Giam Ju Xian <giamjuxian@gmail.com>
2019-01-19 19:04:40 +0800

    Add file upload api

--- functions/index.js
+++ functions/index.js
@@ -1,21 +1,20 @@
-const functions = require('firebase-functions');
-const express = require('express');
+'use strict'
 
-// The Firebase Admin SDK to access the Firebase Realtime Database.
-const admin = require('firebase-admin');
-admin.initializeApp();
+const functions = require('firebase-functions')
+const express = require('express')
+const multer = require('multer')
+const databaseWorker = require('./databaseWorker')
+const upload = multer({dest: '../uploads/'})
+const app = express()
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+var admin = require("firebase-admin")
+var serviceAccount = require("../refreshar-c9d2f-firebase-adminsdk-5rxen-5c249a3823.json");
 
-const app = express();
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://refreshar-c9d2f.firebaseio.com"
+});
 
-app.get('/hello', function(req, res) {
-	res.send("Hello Clarence");
-})
-
-app.get('/clarence', function(req, res) {
-	res.send("Hello Jeffrey")
-})
+app.post('/sendImageToDatabase', upload.single('productImage'), databaseWorker.uploadImage)
 
 exports.app = functions.https.onRequest(app);
