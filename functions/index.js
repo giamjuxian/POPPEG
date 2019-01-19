e1cb6cac18c4a694a4daec1723131ef53a65f19d
Giam Ju Xian <giamjuxian@gmail.com>
2019-01-19 23:57:33 +0800

    Add vuforia integration to web app

--- functions/index.js
+++ functions/index.js
@@ -5,17 +5,39 @@ const express = require('express');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+// load module
+var vuforia = require('vuforiajs');
+
+// init client with valid credentials
+var client = vuforia.client({
+    'accessKey': '4734be8d373164570b5ea5f5421c47df2845a64e',
+    'secretKey': '7d0ea5b4190bc98dca2ed81f6384b069ae641610',
+    'clientAccessKey': '0f207cbf4f3fab96dd48ea99e3a042bc798d9b0c',
+    'clientSecretKey': 'f25ae10ca341c69c354e30a9c429b521020fc46e',
+});
+
+// util for base64 encoding and decoding
+var util = vuforia.util();
 
 const app = express();
 
-app.get('/hello', function(req, res) {
-	res.send("Hello Clarence");
-})
-
-app.get('/clarence', function(req, res) {
-	res.send("Hello Jeffrey")
+app.post('/vuforiaUpload', function(req, res, next) {
+	var targetName = req.body.name;
+	var width = 10;
+	var targetImage = req.body.image;
+	var target = {
+    'name': targetName,
+	    'width': width,
+	    'image': targetImage,
+	    'active_flag': true,
+	};
+	client.addTarget(target, function (error, result) {
+	    if (error) { 
+	        res.status(404).json({error: error});
+	    } else {
+	        res.status(200).json({success: "Image is successfully uploaded"});
+	    }
+	});
 })
 
 exports.app = functions.https.onRequest(app);
