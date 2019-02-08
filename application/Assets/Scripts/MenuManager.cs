557fe94f052c856ed1c650432b6cfb3405c11abe
Giam Ju Xian <giamjuxian@gmail.com>
2019-02-09 01:55:54 +0800

    Migrate vuforia to easyAR

---
+++ application/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject mainPanel;
+    public GameObject downloadPanel;
+    private bool isDownloadPanel;
+
+
+    private void Start()
+    {
+        isDownloadPanel = false;
+    }
+
+    /*
+     * Toggles between download and main menu panels
+     */
+    public void ToggleDownloadPanel()
+    {
+        isDownloadPanel = !isDownloadPanel;
+        mainPanel.SetActive(!isDownloadPanel);
+        downloadPanel.SetActive(isDownloadPanel);
+    }
+
+    /*
+     * Loads camera scene using vuforia
+     */
+    public void LoadCameraScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
