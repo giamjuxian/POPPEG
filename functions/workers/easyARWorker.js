701428f71642febba91486bb630e3fc655680782
giamjuxian <giamjuxian@gmail.com>
2019-03-30 17:50:11 +0800

    Change server to north america region

--- functions/workers/easyARWorker.js
+++ functions/workers/easyARWorker.js
@@ -2,22 +2,22 @@
 
 const config = require('../config.json');
 const request = require('request');
-const easyar_config = config.easyar_config;
+const easyar_new_config = config.easyar_new_config;
 const crypto = require('crypto');
 const EasyARSDK = require('easyar');
-var farmer = EasyARSDK.farmer(easyar_config.serverUrl, easyar_config.cloudKey, easyar_config.cloudSecret);
+var farmer = EasyARSDK.farmer(easyar_new_config.serverUrl, easyar_new_config.cloudKey, easyar_new_config.cloudSecret);
 
-exports.uploadImageToEasyAR = function(req, res, next) {
+exports.uploadImageToEasyAR = function (req, res, next) {
     farmer.createTarget({
         image: req.body.image,
         name: req.body.name,
         size: "20",
         meta: "",
         type: "ImageTarget"
-    }).then(function(response) {
+    }).then(function (response) {
         console.log("Uploaded: " + response.result.targetId);
-        return res.status(200).json({ success: "Image is successfully uploaded to EasyAR" , response: response.result});
-    }).fail(function(error) {
+        return res.status(200).json({ success: "Image is successfully uploaded to EasyAR", response: response.result });
+    }).fail(function (error) {
         var errorMsg = JSON.parse(error.message);
         console.error("Error uploading image to  EasyAR - " + JSON.stringify(errorMsg));
         return res.status(400).json({ error: errorMsg });
