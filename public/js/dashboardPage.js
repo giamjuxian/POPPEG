f2c79c72ebb75647529d19fb4c35450306a13437
Clarence Koh <clarence@nodeflair.com>
2019-07-15 13:46:42 +0900

    adds add card functionality adds success message update card image size

--- public/js/dashboardPage.js
+++ public/js/dashboardPage.js
@@ -4,4 +4,26 @@ $(document).ready(() => {
         $(".arrow-right").toggleClass("show-item");
         $(".arrow-left").toggleClass("hide-item");
     });
+
+    $("#uploadButton").on("click", () => {
+        /** SUCCESS UPLOAD EVENTS */
+
+        // add new card with success uploads into card-wrapper.
+        var $cardWrapper = $(".card-wrapper");
+        $cardWrapper.append(addMediaCard());
+        // Hide modal upon successful upload.
+        $("#addCardModal").modal("toggle");
+        $("#successAlert").show();
+        setTimeout(() => {
+            $("#successAlert").hide();
+        }, 2500);
+    });
 });
+
+function addMediaCard() {
+    let mediaBlock = `<div class="display-card">
+                    <img src="/media/stock_image_1.jpg" alt="Display Image 1">
+                    <div></div>
+                </div>`;
+    return mediaBlock;
+}
