c313075ef9681815d90a46fd0a1be247943a3cd5
Clarence Koh <clarence@nodeflair.com>
2019-07-18 11:31:46 +0900

    remove sidebar update nav bar

--- public/js/dashboardPage.js
+++ public/js/dashboardPage.js
@@ -1,9 +1,4 @@
 $(document).ready(() => {
-    $(".sideMenuToggler").on("click", () => {
-        $(".wrapper").toggleClass("active");
-        $(".arrow-right").toggleClass("show-item");
-        $(".arrow-left").toggleClass("hide-item");
-    });
 
     $("#instructionsClose").on("click", () => {
         $("#uploadRemaining").show();
