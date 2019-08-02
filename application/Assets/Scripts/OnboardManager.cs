286a428738910e02698d529adce4b2ae5dac0464
Jeffrey Halim <jeffreyhalim96@gmail.com>
2019-08-02 11:33:54 +0800

    reimport assets

---
+++ application/Assets/Scripts/OnboardManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class OnboardManager : MonoBehaviour
+{
+    public RectTransform OnBoard1, OnBoard2, OnBoard3, OnBoard4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        OnBoard1.DOAnchorPos(Vector2.zero, 0.25f);
+    }
+
+    // Update is called once per frame
+    public void OnBoard1Next()
+    {
+        OnBoard1.DOAnchorPos(new Vector2(-1080, 0), 0.25f);
+        OnBoard2.DOAnchorPos(new Vector2(0, 0), 0.25f);
+    }
+
+    public void OnBoard2Back()
+    {
+        OnBoard1.DOAnchorPos(new Vector2(0, 0), 0.25f);
+        OnBoard2.DOAnchorPos(new Vector2(1080, 0), 0.25f);
+    }
+
+    public void OnBoard2Next()
+    {
+        OnBoard2.DOAnchorPos(new Vector2(-1080, 0), 0.25f);
+        OnBoard3.DOAnchorPos(new Vector2(0, 0), 0.25f);
+    }
+
+    public void OnBoard3Back()
+    {
+        OnBoard2.DOAnchorPos(new Vector2(0, 0), 0.25f);
+        OnBoard3.DOAnchorPos(new Vector2(1080, 0), 0.25f);
+    }
+
+    public void OnBoard3Next()
+    {
+        OnBoard3.DOAnchorPos(new Vector2(-1080, 0), 0.25f);
+        OnBoard4.DOAnchorPos(new Vector2(0, 0), 0.25f);
+    }
+
+    public void OnBoard4Back()
+        {
+            OnBoard3.DOAnchorPos(new Vector2(0, 0), 0.25f);
+            OnBoard4.DOAnchorPos(new Vector2(1080, 0), 0.25f);
+        }
+}
--- application/Assets/Scripts/SceneHandler.cs
+++ application/Assets/Scripts/SceneHandler.cs
@@ -2,22 +2,105 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Runtime.Serialization.Formatters.Binary;
+//using UnityEngine.Windows;
+using System.IO;
+
 
 public class SceneHandler : MonoBehaviour
 {
-    /*
-     * Loads camera scene using EasyAR
-     */
+    public string sceneID;
+    private int x;
+    private bool func;
+    public int firstTime;
+    bool bar = false;
+    public Scene MenuScene;
+    public Scene OnboardScene;
+
+
     public void LoadCameraScene()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(2);
     }
 
-    /*
-    * Loads camera scene using vuforia
-    */
+
     public void LoadMenuScene()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(1);
+        Debug.Log("menu");
+
     }
+
+    public void LoadOnboardScene()
+    {
+
+        SceneManager.LoadScene(0);
+        Debug.Log("board");
+
+    }
+
+    private void Awake()
+    {
+
+        //Debug.Log("stuck");
+        //if (!PlayerPrefs.HasKey("save"))
+        //{
+        //    PlayerPrefs.SetInt("save", 1);
+        //    LoadMenuScene();
+        //    //StartCoroutine(CheckScene());
+        //    PlayerPrefs.Save();
+        //}
+
+        //else
+        //{
+        //    LoadOnboardScene();
+        //}
+
+    }
+
+    private void Start()
+    {
+        StartCoroutine(CheckScene());
+        Debug.Log("start");
+    }
+
+    IEnumerator CheckScene()
+    {
+
+        if (bar)
+        {
+            yield return null;
+        }
+        bar = true;
+
+        firstTime = 1;
+        // checks first usage of the app
+
+
+        if (firstTime == 1)
+        {
+
+            PlayerPrefs.SetInt("savedFirstTime", 0);
+            SceneManager.SetActiveScene(OnboardScene);
+            firstTime = PlayerPrefs.GetInt("savedFirstTime", 1);
+            //LoadOnboardScene();
+
+        }
+        else
+        {
+            SceneManager.SetActiveScene(MenuScene);
+            //SceneManager.UnloadScene(OnboardScene);
+            //LoadMenuScene();
+            yield return null;
+
+
+
+        }
+
+
+    }
+
+
 }
+
+
